f2d0a666680f48fd3c9d7b9e1aeca31bc8dd7b05
Saiem Gilani <saiem.gilani@gmail.com>
2021-04-18 04:17:11 -0400

    Update test-cfbd_team_info.R

--- tests/testthat/test-cfbd_team_info.R
+++ tests/testthat/test-cfbd_team_info.R
@@ -3,7 +3,7 @@ context("CFB Team Info")
 cols <- c(
   "team_id", "school", "mascot", "abbreviation", "alt_name1", "alt_name2",
   "alt_name3", "conference", "division",
-  "color", "alt_color", "logo_1","logo_2", "venue_id", "venue_name",
+  "color", "alt_color", "logos", "venue_id", "venue_name",
   "city", "state", "zip", "country_code", "timezone", 
   "latitude","longitude","elevation",
   "capacity", "year_constructed", "grass", "dome")
