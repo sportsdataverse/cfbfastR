e6b3887a540fe974c6767a64ed6814c36b1ae081
Saiem Gilani <saiem.gilani@gmail.com>
2021-04-22 17:35:59 -0400

    Update cfbd_teams.R

--- R/cfbd_teams.R
+++ R/cfbd_teams.R
@@ -3,15 +3,15 @@
 #' @title CFBD Teams Endpoint
 #' @description 
 #' \describe{
-#' \item{`cfbd_team_info()`: Team Info Lookup}
-#' \item{`cfbd_team_matchup_records()`: Get matchup history records between two teams.}
-#' \item{`cfbd_team_matchup()`: Get matchup history between two teams.}
-#' \item{`cfbd_team_talent()`: Get composite team talent rankings for all teams in a given year}
+#' \item{`cfbd_team_info()`: Team Info Lookup}{.}
+#' \item{`cfbd_team_matchup_records()`: Get matchup history records between two teams.}{.}
+#' \item{`cfbd_team_matchup()`: Get matchup history between two teams.}{.}
+#' \item{`cfbd_team_roster()`: Get a team's full roster by year.}{.}
+#' \item{`cfbd_team_talent()`: Get composite team talent rankings for all teams in a given year}{.}
 #' }
-#' Team Info Lookup
-#' Lists all teams in conference or all D-I teams if conference is left NULL
-#' Currently, support is only provided for D-I
-#'
+#' ## Team Info Lookup
+#'   Lists all teams in conference or all D-I teams if conference is left NULL
+#'   Currently, support is only provided for D-I
 #' @param conference (\emph{String} optional): Conference abbreviation - Select a valid FBS conference\cr
 #' Conference abbreviations P5: ACC, B12, B1G, SEC, PAC,\cr
 #' Conference abbreviations G5 and FBS Independents: CUSA, MAC, MWC, Ind, SBC, AAC\cr
@@ -19,36 +19,34 @@
 #' If year is left blank while only_fbs is TRUE, then will return values for most current year
 #' @param year (\emph{Integer} optional): Year, 4 digit format (\emph{YYYY}). Filter for getting a list of major division team for a given year
 #' @param verbose Logical parameter (TRUE/FALSE, default: FALSE) to return warnings and messages from function
-#'
 #' @return [cfbd_team_info()] - A data frame with 12 variables:
 #' \describe{
-#'   \item{`team_id`: integer.}
-#'   \item{`school`: character.}
-#'   \item{`mascot`: character.}
-#'   \item{`abbreviation`: character.}
-#'   \item{`alt_name1`: character.}
-#'   \item{`alt_name2`: character.}
-#'   \item{`alt_name3`: character.}
-#'   \item{`conference`: character.}
-#'   \item{`division`: character.}
-#'   \item{`color`: character.}
-#'   \item{`alt_color`: character.}
-#'   \item{`logo_1`: character.}
-#'   \item{`logo_2`: character.}
-#'   \item{`venue_id`: character.}
-#'   \item{`venue_name`: character.}
-#'   \item{`city`: character.}
-#'   \item{`state`: character.}
-#'   \item{`zip`: character.}
-#'   \item{`country_code`: character.}
-#'   \item{`timezone`: character.}
-#'   \item{`latitude`: character.}
-#'   \item{`longitude`: character.}
-#'   \item{`elevation`: character.}
-#'   \item{`capacity`: character.}
-#'   \item{`year_constructed`: character.}
-#'   \item{`grass`: character.}
-#'   \item{`dome`: character.}
+#'   \item{`team_id`: integer.}{Referencing team id.}
+#'   \item{`school`: character.}{Team name.}
+#'   \item{`mascot`: character.}{Team mascot.}
+#'   \item{`abbreviation`: character.}{Team abbreviations.}
+#'   \item{`alt_name1`: character.}{Team alternate name 1 (as it appears in `play_text`).}
+#'   \item{`alt_name2`: character.}{Team alternate name 2 (as it appears in `play_text`).}
+#'   \item{`alt_name3`: character.}{Team alternate name 3 (as it appears in `play_text`).}
+#'   \item{`conference`: character.}{Conference of team.}
+#'   \item{`division`: character.}{Division of team within the conference.}
+#'   \item{`color`: character.}{Team color (primary).}
+#'   \item{`alt_color`: character.}{Team color (alternate).}
+#'   \item{`logos`: character.}{Team logos.}
+#'   \item{`venue_id`: character.}{Referencing venue id.}
+#'   \item{`venue_name`: character.}{Stadium name.}
+#'   \item{`city`: character.}{Team/venue city.}
+#'   \item{`state`: character.}{Team/venue state.}
+#'   \item{`zip`: character.}{Team/venue zip code (someone double check Miami (FL) on if they're in the same zip code).}
+#'   \item{`country_code`: character.}{Team/venue country code.}
+#'   \item{`timezone`: character.}{Team/venue timezone.}
+#'   \item{`latitude`: character.}{Venue latitude.}
+#'   \item{`longitude`: character.}{Venue longitude.}
+#'   \item{`elevation`: character.}{Venue elevation.}
+#'   \item{`capacity`: character.}{Venue capacity.}
+#'   \item{`year_constructed`: character.}{Year the venue was constructed.}
+#'   \item{`grass`: character.}{TRUE/FALSE response on whether the field is grass or not (oh, and there are so many others).}
+#'   \item{`dome`: character.}{TRUE/FALSE flag for if the venue is a domed stadium.}
 #' }
 #' @source \url{https://api.collegefootballdata.com/teams}
 #' @keywords Teams
@@ -66,7 +64,6 @@
 #'
 #' cfbd_team_info(year = 2019)
 #' }
-#'
 cfbd_team_info <- function(conference = NULL, only_fbs = TRUE, year = NULL,
                            verbose = FALSE) {
   if (!is.null(conference)) {
@@ -184,9 +181,8 @@ cfbd_team_info <- function(conference = NULL, only_fbs = TRUE, year = NULL,
 }
 
 
-#' Get matchup history records between two teams.
 #' @rdname cfbd_teams
-#'
+#' @title Get matchup history records between two teams.
 #' @param team1 (\emph{String} required): D-I Team 1
 #' @param team2 (\emph{String} required): D-I Team 2
 #' @param min_year (\emph{Integer} optional): Minimum of year range, 4 digit format (\emph{YYYY})
@@ -195,13 +191,13 @@ cfbd_team_info <- function(conference = NULL, only_fbs = TRUE, year = NULL,
 #'
 #' @return [cfbd_team_matchup_records()] - A data frame with 7 variables:
 #' \describe{
-#'   \item{`start_year`: character.}
-#'   \item{`end_year`: character.}
-#'   \item{`team1`: character.}
-#'   \item{`team1_wins`: character.}
-#'   \item{`team2`: character.}
-#'   \item{`team2_wins`: character.}
-#'   \item{`ties`: character.}
+#'   \item{`start_year`: character.}{Span starting year.}
+#'   \item{`end_year`: character.}{Span ending year.}
+#'   \item{`team1`: character.}{First team selected in query.}
+#'   \item{`team1_wins`: character.}{First team wins in series against `team2`.}
+#'   \item{`team2`: character.}{Second team selected in query.}
+#'   \item{`team2_wins`: character.}{Second team wins in series against `team1`.}
+#'   \item{`ties`: character.}{Number of ties in the series.}
 #' }
 #' @source \url{https://api.collegefootballdata.com/teams/matchup}
 #' @keywords Team Matchup Records
@@ -318,28 +314,26 @@ cfbd_team_matchup_records <- function(team1, team2, min_year = NULL, max_year = 
 }
 
 
-#' Get matchup history between two teams.
 #' @rdname cfbd_teams
-#'
+#' @title Get matchup history between two teams.
 #' @param team1 (\emph{String} required): D-I Team 1
 #' @param team2 (\emph{String} required): D-I Team 2
 #' @param min_year (\emph{Integer} optional): Minimum of year range, 4 digit format (\emph{YYYY})
 #' @param max_year (\emph{Integer} optional): Maximum of year range, 4 digit format (\emph{YYYY})
 #' @param verbose Logical parameter (TRUE/FALSE, default: FALSE) to return warnings and messages from function
-#'
 #' @return [cfbd_team_matchup] - A data frame with 11 variables:
 #' \describe{
-#'   \item{`season`: integer.}
-#'   \item{`week`: integer.}
-#'   \item{`season_type`: character.}
-#'   \item{`date`: character.}
-#'   \item{`neutral_site`: logical.}
-#'   \item{`venue`: character.}
-#'   \item{`home_team`: character.}
-#'   \item{`home_score`: integer.}
-#'   \item{`away_team`: character.}
-#'   \item{`away_score`: integer.}
-#'   \item{`winner`: character.}
+#'   \item{`season`: integer.}{Season the game took place.}
+#'   \item{`week`: integer.}{Game week of the season.}
+#'   \item{`season_type`: character.}{Season type of the game.}
+#'   \item{`date`: character.}{Game date.}
+#'   \item{`neutral_site`: logical.}{TRUE/FALSE flag for if the game took place at a neutral site.}
+#'   \item{`venue`: character.}{Stadium name.}
+#'   \item{`home_team`: character.}{Home team of the game.}
+#'   \item{`home_score`: integer.}{Home score in the game.}
+#'   \item{`away_team`: character.}{Away team of the game.}
+#'   \item{`away_score`: integer.}{Away score in the game.}
+#'   \item{`winner`: character.}{Winner of the matchup.}
 #' }
 #' @source \url{https://api.collegefootballdata.com/teams/matchup}
 #' @keywords Team Matchup
@@ -449,32 +443,31 @@ cfbd_team_matchup <- function(team1, team2, min_year = NULL, max_year = NULL,
 }
 
 
-#' Team Roster
+#' @rdname cfbd_teams
+#' @title Team Roster
 #' Get a teams full roster by year. If team is not selected, API returns rosters for every team from the selected year.
 #'
-#' @rdname cfbd_teams
 #' @param year (\emph{Integer} required): Year,  4 digit format (\emph{YYYY})
 #' @param team (\emph{String} optional): Team, select a valid team in D-I football
 #' @param verbose Logical parameter (TRUE/FALSE, default: FALSE) to return warnings and messages from function
 #'
-#'
 #' @return [cfbd_team_roster()] - A data frame with 12 variables:
 #' \describe{
-#'   \item{`athlete_id`: character.}
-#'   \item{`first_name`: character.}
-#'   \item{`last_name`: character.}
-#'   \item{`team`: character.}
-#'   \item{`weight`: integer.}
-#'   \item{`height`: integer.}
-#'   \item{`jersey`: integer.}
-#'   \item{`year`: integer.}
-#'   \item{`position`: character.}
-#'   \item{`home_city`: character.}
-#'   \item{`home_state`: character.}
-#'   \item{`home_country`: character.}
-#'   \item{`home_latitude`: numeric.}
-#'   \item{`home_longitude`: number.}
-#'   \item{`home_county_fips`: integer.}
+#'   \item{`athlete_id`: character.}{Referencing athlete id.}
+#'   \item{`first_name`: character.}{Athlete first name.}
+#'   \item{`last_name`: character.}{Athlete last name.}
+#'   \item{`team`: character.}{Team name.}
+#'   \item{`weight`: integer.}{Athlete weight.}
+#'   \item{`height`: integer.}{Athlete height.}
+#'   \item{`jersey`: integer.}{Athlete jersey number.}
+#'   \item{`year`: integer.}{Athlete year.}
+#'   \item{`position`: character.}{Athlete position.}
+#'   \item{`home_city`: character.}{Hometown of the athlete.}
+#'   \item{`home_state`: character.}{Hometown state of the athlete.}
+#'   \item{`home_country`: character.}{Hometown country of the athlete.}
+#'   \item{`home_latitude`: numeric.}{Hometown latitude.}
+#'   \item{`home_longitude`: number.}{Hometown longitude.}
+#'   \item{`home_county_fips`: integer.}{Hometown FIPS code.}
 #' }
 #' @source \url{https://api.collegefootballdata.com/roster}
 #' @keywords Team Roster
@@ -571,9 +564,9 @@ cfbd_team_roster <- function(year, team = NULL,
 #'
 #' @return [cfbd_team_talent()] - A data frame with 3 variables:
 #' \describe{
-#'   \item{`year`: integer.}
-#'   \item{`school`: character.}
-#'   \item{`talent`: double.}
+#'   \item{`year`: integer.}{Season for the talent rating.}
+#'   \item{`school`: character.}{Team name.}
+#'   \item{`talent`: double.}{Overall roster talent points (as determined by 247Sports).}
 #' }
 #' @source \url{https://api.collegefootballdata.com/talent}
 #' @keywords Team talent
