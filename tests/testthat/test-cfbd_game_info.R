37fd928f6ef46af9448b934e906970f559443d67
Saiem Gilani <saiem.gilani@gmail.com>
2021-06-29 12:40:44 -0400

    All the way back

--- tests/testthat/test-cfbd_game_info.R
+++ tests/testthat/test-cfbd_game_info.R
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 context("CFB Game Info")
 
 
@@ -22,3 +23,29 @@ test_that("CFB Game Info", {
   expect_s3_class(x, "data.frame")
   expect_s3_class(y, "data.frame")
 })
+=======
+context("CFB Game Info")
+
+
+cols <- c(
+  "game_id", "season", "week", "season_type", "start_date",
+  "start_time_tbd", "neutral_site", "conference_game",
+  "attendance", "venue_id", "venue",
+  "home_id", "home_team", "home_conference",
+  "home_points", "home_post_win_prob",
+  "away_id", "away_team", "away_conference",
+  "away_points", "away_post_win_prob", "excitement_index",
+  "highlights"
+)
+
+test_that("CFB Game Info", {
+  skip_on_cran()
+  x <- cfbd_game_info(2019, week = 1, conference = "ACC")
+  
+  y <- cfbd_game_info(2018, week = 4, conference = "Ind")
+  expect_equal(colnames(x), cols)
+  expect_equal(colnames(y), cols)
+  expect_s3_class(x, "data.frame")
+  expect_s3_class(y, "data.frame")
+})
+>>>>>>> Stashed changes
