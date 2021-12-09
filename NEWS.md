474db58ae4ff08bdbea8054d274cfc0bf68a966a
Kazink36 <13jaredlee@gmail.com>
2021-12-08 19:54:23 -0700

    add cfbd_player_portal() function

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,6 @@
+# **cfbfastR v1.7.0**
+
+### Added [```cfbd_player_portal()```](https://saiemgilani.github.io/cfbfastR/reference/cfbd_player_portal.html)
 
 # **cfbfastR v1.6.7**
 
---
+++ tests/testthat/test-cfbd_player_portal.R
@@ -0,0 +1,14 @@
+context("CFB Transfer Portal")
+
+
+cols <- c(
+  "athlete_id", "season", "first_name", "last_name", "position", "origin", "destination", "transfer_date",
+  "rating", "stars", "eligibility"
+)
+
+test_that("CFB Transfer Portal", {
+  skip_on_cran()
+  x <- cfbd_player_portal(year = 2021)
+  expect_equal(colnames(x), cols)
+  expect_s3_class(x, "data.frame")
+})
