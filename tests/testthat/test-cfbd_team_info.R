45ec8c6995e8a3d2635425854c43a769492a018d
saiemgilani <saiem.gilani@gmail.com>
2022-09-22 23:15:47 -0400

    Update test-cfbd_team_info.R

--- tests/testthat/test-cfbd_team_info.R
+++ tests/testthat/test-cfbd_team_info.R
@@ -2,7 +2,7 @@
 cols <- c(
   "team_id", "school", "mascot", "abbreviation", "alt_name1", "alt_name2",
   "alt_name3", "conference", "division",
-  "color", "alt_color", "logo", "logo_2", "venue_id", "venue_name",
+  "color", "alt_color", "logo", "logo_2", "twitter", "venue_id", "venue_name",
   "city", "state", "zip", "country_code", "timezone",
   "latitude","longitude","elevation",
   "capacity", "year_constructed", "grass", "dome")
