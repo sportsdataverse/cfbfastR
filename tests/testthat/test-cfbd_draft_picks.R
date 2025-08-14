cd55ca1b3d6932c85b192f71ad9bf13f353820c8
BradHill <bradhill119@gmail.com>
2025-08-14 00:05:49 -0500

    add expected nfl_team_id field

--- tests/testthat/test-cfbd_draft_picks.R
+++ tests/testthat/test-cfbd_draft_picks.R
@@ -3,7 +3,7 @@
 cols <- c(
   "college_athlete_id", "nfl_athlete_id",
   "college_id","college_team","college_conference",
-  "nfl_team","year","overall","round","pick","name", "position",
+  "nfl_team_id", "nfl_team","year","overall","round","pick","name", "position",
   "height", "weight",
   "pre_draft_ranking", "pre_draft_position_ranking", "pre_draft_grade",
   "hometown_info_city", "hometown_info_state", "hometown_info_country","hometown_info_latitude",
