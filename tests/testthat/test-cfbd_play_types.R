f9704f994dc2cfc98904258c371ee05d15740485
Akshay Easwaran <akeaswaran@me.com>
2026-01-04 13:15:08 -0500

    fix tests

--- tests/testthat/test-cfbd_play_types.R
+++ tests/testthat/test-cfbd_play_types.R
@@ -5,7 +5,7 @@ cols <- c("play_type_id", "text", "abbreviation")
 test_that("CFB Play Types", {
   skip_on_cran()
   x <- cfbd_play_types()
-  expect_equal(nrow(x), 48)
+  expect_equal(nrow(x), 49)
   expect_equal(ncol(x), 3)
   expect_setequal(colnames(x), cols)
   expect_s3_class(x, "data.frame")
