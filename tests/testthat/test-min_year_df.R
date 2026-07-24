e5efd00891624452d5718b0120650a89b8e527b2
BradHill <bradhill119@gmail.com>
2026-07-24 13:21:39 -0500

    chore: add skip_on_ci() to test

--- tests/testthat/test-min_year_df.R
+++ tests/testthat/test-min_year_df.R
@@ -1,6 +1,7 @@
 testthat::test_that(
   "Minimum year map has been updated within last 3 months",{
     skip_on_cran()
+    skip_on_ci()
     skip_if(!has_cfbd_key(), "CFBD API key not available")
 
     outdated_mins <- min_year_map_df$last_updated < Sys.Date() - months(3)
